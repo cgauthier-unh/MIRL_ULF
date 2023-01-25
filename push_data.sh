69488b0116b852d4beba9cf1a33f3e1dd221f8d8
cgauthier-unh <123567466+cgauthier-unh@users.noreply.github.com>
2023-01-25 18:08:01 -0500

    Update push_data.sh

--- push_data.sh
+++ push_data.sh
@@ -10,6 +10,9 @@ Dest_DIR="/home/mirl-ulf/ULF/Data_Files/UNH_incoming"
 #sleep 3600 &
 while true; do
 	#sshpass -p "mirlmirl" rsync -aq $Data_DIR $username@$MIRL_IP:$Dest_DIR &
-	sshpass -p "mirlmirl" rsync -aq /home/pi-unh-daq/ULF/Data_Files/ mirl-ulf@132.177.207.87:'/home/mirl-ulf/ULF/Data_Files/UNH_incoming/'
+	if nc -z 132.177.207.87 22 2>/dev/null;
+	then
+	    sshpass -p "mirlmirl" rsync -aq /home/pi-unh-daq/ULF/Data_Files/ mirl-ulf@132.177.207.87:'/home/mirl-ulf/ULF/Data_Files/UNH_incoming/'
+	fi
 	sleep 1d
 done
