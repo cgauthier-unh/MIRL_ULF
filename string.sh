4c1b5f6c44b18b29c6e362a0934207e6eec6a20f
cgauthier-unh <123567466+cgauthier-unh@users.noreply.github.com>
2023-02-07 10:44:15 -0500

    Update string.sh

--- string.sh
+++ string.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
+FILE="/home/pi-unh-daq/ULF/NMEA_String.txt"
+echo " " > /home/pi-unh-daq/ULF/pigs_output.txt
+echo " " > /home/pi-unh-daq/ULF/NMEA_String.txt
+
+let flag=0
+
+while [ $flag == 0 ]; do
 pigs slro 17 9600 8
 sleep 1
 pigs -a slr 17 600 >/home/pi-unh-daq/ULF/pigs_output.txt
 
-FILE="/home/pi-unh-daq/ULF/NMEA_String.txt"
-#echo $FILE
 while read -r line; do
     IFS=","
     read -ra arr <<< "$line"
-    if [[ ${arr[0]} == '$GPRMC' ]] ; 
+    if [ "${arr[0]}" == '$GPRMC' ] && [ "${#arr[@]}" == 13 ] ;
     then
         echo "$line" > $FILE
-#	echo "$line"
+        let flag=1
     fi
 done < /home/pi-unh-daq/ULF/pigs_output.txt
 
 pigs slrc 17
 
+done
